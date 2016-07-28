0c03fbcd72791ac2d6cdafb875b312ed1c7d5756
Aman <bzcheeseman@gmail.com>
2016-07-27 23:50:38 -0500

    updated the serial interface stuff - hopefully it'll work better now.  Still can't get the thread hanging thing to work properly, exit doesn't want to call...

--- scripts/interface_test.py
+++ scripts/interface_test.py
@@ -1,23 +1,70 @@
+import logging
 import socket
 import sys
+import time
 
+MAX_LENGTH = 512
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format = '%(asctime)s %(name)s %(levelname)s %(message)s',
+                        filename = "../logging/MotorControllerClient.log",
+                        filemode = 'w')
 
-HOST = '127.0.0.1'
-CMD_PORT = 10000
+    ip = 'localhost'
+    port = 10000
 
-s_cmd = socket.socket()
+    logger = logging.getLogger('client')
+    logger.info('Server on %s:%s', ip, port)
 
-s_cmd.connect((HOST, CMD_PORT))
+    # Connect to the server
+    logger.debug('creating socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug('connecting to server')
+    s.connect((ip, port))
 
+    # Send the data
+    message = 'calibrate'
+    logger.debug('sending data: "%s"', message)
+    len_sent = s.send(message)
 
-try:
-    msg = raw_input("Command To Send: ")
-    if msg == "close":
-        s_cmd.send("END")
-        s_cmd.close()
-        sys.exit(0)
-    else:
-        s_cmd.sendall(msg + "\n")
-        print s_cmd.recv(1024)
-finally:
-    s_cmd.close()
+    # Receive a response
+    logger.debug('waiting for response')
+    response = s.recv(MAX_LENGTH)
+    logger.debug('response from server: "%s"', response)
+
+    # s.close()
+    #
+    # Connect to the server
+    logger.debug('creating socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug('connecting to server')
+    s.connect((ip, port))
+
+    # Try an invalid message
+    message = 'hello world'
+    logger.debug('sending data: "%s"', message)
+    len_sent = s.send(message)
+
+    # Receive a response
+    logger.debug('waiting for response')
+    response = s.recv(MAX_LENGTH)
+    logger.debug('response from server: "%s"', response)
+
+    s.close()
+
+    # Connect to the server
+    logger.debug('creating socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug('connecting to server')
+    s.connect((ip, port))
+
+    # close the server
+    s.send("close")
+
+    # Clean up
+    logger.debug('closing socket')
+    s.close()
+    logger.debug('done')
+
+if __name__ == '__main__':
+    main()
